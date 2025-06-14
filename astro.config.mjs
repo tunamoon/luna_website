b92ed32e3b9093cfde6589560bfc87da3b4d3260
tunamoon <tunamoon@sas.upenn.edu>
2025-06-14 14:50:04 -0400

    changed navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
 
+const REPO = "luna_website";
 export default defineConfig({
   integrations: [tailwind(), react()],
+  base: `/${REPO}/`,
   outDir: 'docs',
-  base: '/luna_website/',
-  trailingSlash: 'always',
-
 });
